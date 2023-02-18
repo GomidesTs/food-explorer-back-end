b4eb2f729d64ba261fd69ffc793788d5109acd82
GomidesTs <gomidestulio5@gmail.com>
2023-02-18 16:18:54 -0300

    :sparkles: feat: environmt variables and error message
    Inserting the environment application, applying the error response standard

--- src/server.js
+++ src/server.js
@@ -1,8 +1,30 @@
+require('dotenv/config')
+require('express-async-errors')
+
 const express = require('express')
 
+const AppError = require('./utils/AppError')
+
 const app = express()
-const PORT = 3334
+
+const PORT = process.env.PORT || 3334
 
 app.use(express.json())
+app.use((error, request, response, next) => {
+    if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message
+        })
+    }
+
+    console.error(error)
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+    })
+
+})
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
