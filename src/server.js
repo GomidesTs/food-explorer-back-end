1c7794ceb698e7ce27251e62d1c7e8382e1479df
GomidesTs <gomidestulio5@gmail.com>
2023-03-09 22:12:00 -0300

    :sparkles: feat: routes and database
    Connecting the database to server.js and showing the application the routes for modifying data information

--- src/server.js
+++ src/server.js
@@ -3,13 +3,19 @@ require('express-async-errors')
 
 const express = require('express')
 
+const routes = require("./routes")
 const AppError = require('./utils/AppError')
-
+const database = require("./database/sqlite")
 const app = express()
 
 const PORT = process.env.PORT || 3334
 
 app.use(express.json())
+
+app.use(routes)
+
+database()
+
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
