f75328b834bd862a280703f2f0df9f663109fb55
GomidesTs <gomidestulio5@gmail.com>
2023-03-11 13:26:25 -0300

    :sparkles: feat: created files and functions for creating and updating users
    This change adds the users.routes.js, UsersController.js, UsersService.js, and UsersRepository.js files to the project. These files contain functions responsible for handling the creation and updating of users on the system. users.routes.js defines the routes for these functions, while UsersController.js implements controller logic to handle requests. UsersService.js manages business logic and input validations, while UsersRepository.js manages database access and handles user creation and update operations.

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,31 @@
+const UsersRepository = require('../repositories/UsersRepository')
+const UsersService = require('../services/UsersService')
+
+class UsersController {
+    async create(request, response) {
+        const { name, email, password } = request.body
+
+        const usersRepository = new UsersRepository()
+        const usersService = new UsersService(usersRepository)
+
+        await usersService.create({ name, email, password })
+
+        response.status(201).json()
+    }
+
+    async update (request, response) {
+        const {name, email, password, oldPassword} = request.body
+        const {id} = request.query
+        
+        const usersRepository = new UsersRepository()
+        const usersService = new UsersService(usersRepository)
+
+        await usersService.update({id, name, email, password, oldPassword})
+
+        response.status(200).json()
+    }
+
+
+}
+
+module.exports = UsersController
