44ccc4947464bc681ac1e21db60d5766c653ffad
GomidesTs <gomidestulio5@gmail.com>
2023-03-13 20:51:56 -0300

    :bug: fix: fixed image upload location
    This change fixes the location of uploading images in the application. Previously, images were not being saved in the tmp/uploads folder.
    
    Now the images will be saved in the uploads folder, which is located at the root of the project. To do this, we updated the disk storage provider configuration in the upload.js file to use the correct path for the uploads folder.
    
    This correction is important to ensure that the images are stored correctly and can be easily accessed by the application when needed.

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -2,8 +2,8 @@ const path = require('path')
 const multer = require('multer')
 const crypto = require('crypto')
 
-const TMP_FOLDER = path.resolve(__dirname, '..', 'tpm')
-const UPLOADS_FOLDER = path.resolve(TPM_FOLDER, 'uploads')
+const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp')
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, 'uploads')
 
 const BYTE_LENGTH = 16
 
