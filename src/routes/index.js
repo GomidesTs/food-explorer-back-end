833d648ce1e2e2dec989e6bf655c1a6473fcaa4a
GomidesTs <gomidestulio5@gmail.com>
2023-03-21 17:44:44 -0300

    :sparkles: feat: adding all routes
    This commit adds all routes available in the application to the index.js file in the routes folder. This ensures that all routes are available in a single file, making the code easier to navigate and maintain. With this updated file, the routes will be available to the entire application and can be easily accessed by other modules. This change provides a clearer organization of the code and makes it easier to add new routes in the future.

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const { Router } = require('express')
+
+const usersRouter = require('./users.routes')
+const sessionsRouter = require('./sessions.routes')
+const dishesRouter = require('./dishes.routes')
+const ordersRoutes = require('./orders.routes')
+
+const routes = Router()
+
+routes.use('/users', usersRouter)
+routes.use('/sessions', sessionsRouter)
+routes.use('/dishes', dishesRouter)
+routes.use('/orders', ordersRoutes)
+
+module.exports = routes
