61a4ca7a57fd98db264d581ecf91f27fa6804286
GomidesTs <gomidestulio5@gmail.com>
2023-03-21 17:40:50 -0300

    :sparkles: feat: adding orders
    This commit adds the OrdersRoutes.js, OrdersController.js, OrdersService.js, and OrdersRepository.js files to the repository. These files provide the necessary functionality to manage orders in our system.

---
+++ src/services/OrdersService.js
@@ -0,0 +1,61 @@
+const AppError = require('../utils/AppError')
+
+class OrdersService {
+  constructor(ordersRepository) {
+    this.ordersRepository = ordersRepository
+  }
+
+  async create({ user_id, cart, orderStatus, totalPrice, paymentMethod }) {
+    const order_id = await this.ordersRepository.createOrder({ user_id, orderStatus, totalPrice, paymentMethod })
+
+    const orderItems = cart.map(item => {
+      return {
+        title: item.title,
+        quantity: item.quantity,
+        dish_id: item.id,
+        order_id
+      }
+    })
+
+    await this.ordersRepository.itemsInsert(orderItems)
+
+    return orderItems
+  }
+
+  async update({ id, orderStatus }) {
+    const checkOrderExists = await this.ordersRepository.show({ id })
+
+    if (!checkOrderExists) {
+      throw new AppError('Pedido inexistente')
+    }
+
+    const orderUpdated = await this.ordersRepository.update({ id, orderStatus })
+
+    return orderUpdated
+  }
+
+  async index({ user_id }) {
+    const checkUserIsAdmin = await this.ordersRepository.showUser({ id: user_id })
+    const ordersItems = await this.ordersRepository.indexOrdersItems()
+    let orders
+
+    if (checkUserIsAdmin) {
+      orders = await this.ordersRepository.indexOrdersAdmin()
+    } else {
+      orders = await this.ordersRepository.indexOrdersNoAdmin({ user_id })
+    }
+
+    const ordersWithItems = orders.map(order => {
+      const orderItem = ordersItems.filter(item => item.order_id === order.id)
+
+      return {
+        ...order,
+        items: orderItem
+      }
+    })
+
+    return ordersWithItems
+  }
+}
+
+module.exports = OrdersService
