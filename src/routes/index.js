39d5d2268d94504aa466b21bb6be116c3e819651
GomidesTs <gomidestulio5@gmail.com>
2023-05-03 20:01:55 -0300

    :sparkles: feat: carts
    This update added the carts route to the index.js file in the routes folder, allowing the previously created shopping cart functionality to be used in the application. By inserting this route, it will be possible to manage the items added to the cart by the users, allowing the visualization of the selected products, quantity, price, in addition to the finalization of the purchase.

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const sessionsRouter = require('./sessions.routes')
 const dishesRouter = require('./dishes.routes')
 const ordersRoutes = require('./orders.routes')
 const favoritesRoutes = require('./favorites.routes')
-
+const cartsRoutes = require('./carts.routes')
 const routes = Router()
 
 routes.use('/users', usersRouter)
@@ -13,5 +13,6 @@ routes.use('/sessions', sessionsRouter)
 routes.use('/dishes', dishesRouter)
 routes.use('/orders', ordersRoutes)
 routes.use('/favorites', favoritesRoutes)
+routes.use('/carts', cartsRoutes)
 
 module.exports = routes
