ea8f27307869ed0c01daca8bfc79985ee6916660
GomidesTs <gomidestulio5@gmail.com>
2023-05-06 00:11:23 -0300

    :bricks: ci: foreign key
    This update added 'PRAGMA foreign_keys=ON' to the Knex configuration file to allow manipulation of foreign key data. With this setting enabled, SQLite will ensure that all foreign keys in a table are referencing a primary key in another table before allowing any modifications to the data.

--- knexfile.js
+++ knexfile.js
@@ -4,32 +4,43 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, 'src', 'database', 'database.db')
+      filename: path.resolve(__dirname,'src','database','database.db'),
+      foreign_keys: true
     },
     pool: {
-      min: 2,
-      max: 10,
-      afterCreate: (connection, callback) => connection.run('PRAGMA foreign_keys = ON', callback)
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb)
     },
+    useNullAsDefault: true,
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
-    },
-    useNullAsDefault: true
+    }
   },
-
+  staging: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname,'src','database','database.db'),
+    },
+    pool: {
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb)
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
+    }
+  },
   production: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, 'src', 'database', 'database.db')
+      filename: path.resolve(__dirname,'src','database','database.db'),
+      foreign_keys: true
     },
     pool: {
-      min: 2,
-      max: 10,
-      afterCreate: (connection, callback) => connection.run('PRAGMA foreign_keys = ON', callback)
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb)
     },
+    useNullAsDefault: true,
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
-    },
-    useNullAsDefault: true
+    }
   }
+
 }
