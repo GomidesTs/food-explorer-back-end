42ce8086f2c63b65cd5dba625933b8cf0ee671a2
GomidesTs <gomidestulio5@gmail.com>
2023-05-07 11:42:20 -0300

    :sparkles: feat: knex
    this update insert the knex config file the line GRANT ALL PRIVILEGES ON database

--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -3,4 +3,6 @@ const knex = require('knex')
 
 const connection = knex(config.development)
 
+knex.raw('GRANT ALL PRIVILEGES ON database.* TO `%`@`%` IDENTIFIED BY password;')
+
 module.exports = connection
