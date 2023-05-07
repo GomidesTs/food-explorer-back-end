1349350a1caa96c087e849adef48deb5d1959518
GomidesTs <gomidestulio5@gmail.com>
2023-05-07 12:16:59 -0300

    :boom: fix: knex
    This update removes the line GRANT ALL PRIVILEGES ON database from the knex configuration file, as it was giving an error when running the server

--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -3,6 +3,4 @@ const knex = require('knex')
 
 const connection = knex(config.development)
 
-knex.raw('GRANT ALL PRIVILEGES ON database.* TO `%`@`%` IDENTIFIED BY password;')
-
 module.exports = connection
