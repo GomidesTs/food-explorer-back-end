26112056870e5b78383358e9197cba42360d94e8
GomidesTs <gomidestulio5@gmail.com>
2023-05-07 18:06:01 -0300

    :sparkles: feat: user
    This update enters a standard user with administrator authority into the database as soon as the system is started. This means that, when starting the application, the administrator user will already be available in the database, allowing him to manage and configure the application appropriately.

--- src/database/sqlite/index.js
+++ src/database/sqlite/index.js
@@ -1,12 +1,23 @@
 const sqlite = require('sqlite')
 const sqlite3 = require('sqlite3')
 const path = require('path')
+const knex = require('knex')
 
 async function sqliteConnection() {
     const database = await sqlite.open({
         filename: path.resolve(__dirname, '..', 'database.db'),
         driver: sqlite3.Database
     })
+
+    knex.insert({
+        name: 'rocketseat',
+        email: 'admin@foodexplorer.com',
+        password: 'rocketseat',
+        avatar: null,
+        isAdmin: true
+    }).into('users')
+        .then(() => console.log('Usuário criado com sucesso'))
+        .catch((err) => console.error(err))
 
     return database
 }
