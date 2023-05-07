36c695756b46e4a0eef908ff75b08d200f8f7fba
GomidesTs <gomidestulio5@gmail.com>
2023-05-07 18:17:08 -0300

    :sparkles: feat: users
    This update changes the access path to the Knex module, plus it adds a check to verify that the database has already been created before creating the admin user. This is done to ensure the database is available before creating the user with administrator privileges. This check is important because if the database has not yet been created, trying to create the admin user would result in an error. With this change, the system becomes more robust and avoids possible failures.

--- src/database/sqlite/index.js
+++ src/database/sqlite/index.js
@@ -1,7 +1,7 @@
 const sqlite = require('sqlite')
 const sqlite3 = require('sqlite3')
 const path = require('path')
-const knex = require('knex')
+const knex = require('../knex')
 
 async function sqliteConnection() {
     const database = await sqlite.open({
@@ -9,15 +9,21 @@ async function sqliteConnection() {
         driver: sqlite3.Database
     })
 
-    knex.insert({
-        name: 'rocketseat',
-        email: 'admin@foodexplorer.com',
-        password: 'rocketseat',
-        avatar: null,
-        isAdmin: true
-    }).into('users')
-        .then(() => console.log('Usuário criado com sucesso'))
-        .catch((err) => console.error(err))
+    const tableExists = await database.get(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='users'"
+    )
+
+    if (tableExists) {
+        knex.insert({
+            name: 'rocketseat',
+            email: 'admin@foodexplorer.com',
+            password: 'rocketseat',
+            avatar: null,
+            isAdmin: true
+        }).into('users')
+            .then(() => console.log('Usuário criado com sucesso'))
+            .catch((err) => console.error(err))
+    }
 
     return database
 }
